47d37729f4c2b5a76c609ea817d27c4639ed5cac
Andrei-David Nan <nandavid190@gmail.com>
2023-06-23 18:05:22 +0300

    defined the User and HttpResponse domains;

---
+++ src/main/java/com/customerdash/customerdashbackend/CustomersDashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.customerdash.customerdashbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersDashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersDashBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customerdash/customerdashbackend/CustomersDashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customerdash.customerdashbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersDashBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
